68dbee995b8e318358d9a6ce427881378c1941ea
Putra Prasetya <putraprasetya333@gmail.com>
2024-05-30 19:01:31 +0700

    Unit Test

--- src/user/user/user.controller.spec.ts
+++ src/user/user/user.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { UserController } from './user.controller'
+import * as httpMock from 'node-mocks-http'
 
 describe('UserController', () => {
   let controller: UserController
@@ -12,7 +13,19 @@ describe('UserController', () => {
     controller = module.get<UserController>(UserController)
   })
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined()
+  it('should can say hello', async () => {
+    const response = await controller.sayHello('Putra', 'Prasetya')
+    expect(response).toBe('Hello Putra Prasetya')
+  })
+
+  it('should can view template', async () => {
+    const response = httpMock.createResponse()
+    controller.viewHello('Putra', response)
+
+    expect(response._getRenderView()).toBe('index.html')
+    expect(response._getRenderData()).toEqual({
+      name: 'Putra',
+      title: 'Template Engine'
+    })
   })
 })
