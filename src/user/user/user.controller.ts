2fd089b5e691373623bd95fb5b3166bd05300fd2
Putra Prasetya <putraprasetya333@gmail.com>
2024-05-30 11:56:11 +0700

    Request API

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get, Post, Query, Param } from '@nestjs/common'
+// import { Request } from 'express'
 
-@Controller('user')
-export class UserController {}
+@Controller('/api/users')
+export class UserController {
+  @Get('/sample')
+  get(): string {
+    return 'GET'
+  }
+
+  @Get('/hello')
+  sayHello(
+    @Query('firstname') firstName:string,
+    @Query('lastname') lastName: string
+  ): string {
+    return `Hello ${firstName} ${lastName}`
+  }
+
+  // @Get('/:id')
+  // getById(@Req() request: Request): string {
+  //   return `GET ${request.params.id}`
+  // }
+
+  @Get('/:id')
+  getById(@Param('id') id: string): string {
+    return `GET ${id}`
+  }
+
+  @Post()
+  post(): string {
+    return 'POST'
+  }
+}
