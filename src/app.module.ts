90a558b359823c3ac990d733000b0388a10f780d
Putra Prasetya <putraprasetya333@gmail.com>
2024-06-07 21:02:55 +0700

    Logging

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,16 @@ import { AppService } from './app.service'
 import { UserModule } from './user/user.module'
 import { ConfigModule } from '@nestjs/config'
 import { PrismaModule } from './prisma/prisma.module'
+import * as winston from 'winston'
+import { WinstonModule } from 'nest-winston'
 
 @Module({
   imports: [
+    WinstonModule.forRoot({
+      format: winston.format.json(),
+      level: 'debug',
+      transports: [new winston.transports.Console()]
+    }),
     ConfigModule.forRoot({
       isGlobal: true
     }),
--- src/user/user-repository/user-repository.ts
+++ src/user/user-repository/user-repository.ts
@@ -1,14 +1,20 @@
-import { Injectable } from '@nestjs/common'
+import { Inject, Injectable } from '@nestjs/common'
 import { User } from '@prisma/client'
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston'
 import { PrismaService } from 'src/prisma/prisma.service'
+import { Logger } from 'winston'
 
 @Injectable()
 export class UserRepository {
-  constructor(private prismaService: PrismaService) {
-    console.info('Create user repository')
+  constructor(
+    private prismaService: PrismaService,
+    @Inject(WINSTON_MODULE_PROVIDER) private logger: Logger
+  ) {
+    this.logger.info('Create user repository')
   }
 
   async save(firstName: string, lastName?: string): Promise<User> {
+    this.logger.info(`create user with firstName ${firstName} and lastName ${lastName}`)
     return this.prismaService.user.create({
       data: {
         first_name: firstName,
